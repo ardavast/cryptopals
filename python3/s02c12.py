e63bd3c93d5c5dd86bacf8146efb89a7f3470ae8
Ardavast Dayleryan <ardavast@noiseoverip.com>
2017-02-26 10:03:08 +0200

    Commit challenge 12 from set 2

---
+++ python3/s02c12.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+"""
+Byte-at-a-time ECB decryption (Simple).
+https://cryptopals.com/sets/2/challenges/12
+"""
+
+import os
+from base64 import b64decode
+
+from block import pkcs7_pad, ecb_encrypt, ecb_detect
+
+KEYSIZE = 16
+k = os.urandom(KEYSIZE)
+u = ('Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4g'
+     'YmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQg'
+     'eW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK')
+u = b64decode(u)
+
+
+def encryption_oracle(p):
+    return ecb_encrypt(pkcs7_pad(p + u, 16), k)
+
+
+def guess_blocksize():
+    csize0 = len(encryption_oracle(b''))
+    psize = 1
+    while True:
+        p = b'X' * psize
+        csize = len(encryption_oracle(p))
+        if csize - csize0 > 0:
+            return csize - csize0
+        psize += 1
+
+
+def ecb_crack_block(blocksize, p):
+    target_bytes = slice(len(p), len(p) + blocksize)
+    pblock = b''
+    for i in range(1, blocksize + 1):
+        crafted_block = b'X' * (blocksize - i)
+        c0 = encryption_oracle(crafted_block)
+        for b in range(0, 255 + 1):
+            b = bytes([b])
+            c = encryption_oracle(crafted_block + p + pblock + b)
+            if c[target_bytes] == c0[target_bytes]:
+                pblock += b
+                break
+    return pblock
+
+
+blocksize = guess_blocksize()
+if ecb_detect(encryption_oracle(b'X' * blocksize * 2)):
+    p = b''
+    for i in range(len(encryption_oracle(b'')) // blocksize):
+        p += ecb_crack_block(blocksize, p)
+    p = p[:-1]
+    print(p.decode('utf8'))
